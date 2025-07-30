2a2ba86b2978a68c68fdb4cf0eaa291e0496cdab
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-07-30 18:45:40 -0300

    feat: cria e configura relatorio_controller e registra blueprint no app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from controllers.pedido_controller import pedido_bp
+from controllers.relatorio_controller import relatorio_bp
 
 app = Flask(__name__)
 app.register_blueprint(pedido_bp)
+app.register_blueprint(relatorio_bp)
+
 app.run(host="localhost", port=5000, debug=True)
---
+++ controllers/relatorio_controller.py
@@ -0,0 +1,3 @@
+from flask import request, Blueprint
+
+relatorio_bp = Blueprint("ralatorio", __name__)

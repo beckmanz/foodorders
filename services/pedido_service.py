7eccc9957caae2ecb8b1e28a988809042c9af3a3
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-07-30 18:24:51 -0300

    feat: cria método salvar_pedido para fazer o salvamento dos pedidos no datalake local

--- services/pedido_service.py
+++ services/pedido_service.py
@@ -10,4 +10,27 @@ class PedidoService:
         if not os.path.exists(self.arquivo):
             with open(self.arquivo, "w", newline="") as file:
                 writer = csv.writer(file)
-                writer.writerow(["id_pedido", "restaurante", "valor", "tempo_entrega", "status", "cidade", "data_hora"])  
+                writer.writerow(["id_pedido", "restaurante", "valor", "tempo_entrega", "status", "cidade", "data_hora"])
+
+    def salvar_pedido(self, pedido):
+
+        with open(self.arquivo, "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([
+                pedido.id_pedido,
+                pedido.restaurante,
+                pedido.valor,
+                pedido.tempo_entrega,
+                pedido.status,
+                pedido.cidade,
+                pedido.data_hora
+            ])
+        
+        res = make_response(
+            jsonify(
+                message="Pedido salvo com sucesso!",
+                pedido = pedido.__dict__
+            )
+        )
+        return res
+        

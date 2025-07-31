b9e0c8dee07c5e395c786bb5e9f57527f4580f66
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-07-31 16:06:35 -0300

    feat: cria método para gerar relatorios dos restaurantes com mais pedidos

--- services/relatorio_service.py
+++ services/relatorio_service.py
@@ -1,2 +1,46 @@
+from flask import jsonify, make_response
+import pandas as pd
+import os
+CSV_PATH = 'data/pedidos.csv'
+
 class RelatorioService:
-    pass
+    def gerar_relatorio_top_restaurante(self, quantidade):
+        quantidade = min(max(1, quantidade), 100)
+
+        if not os.path.exists(CSV_PATH):
+            return make_response(
+                jsonify({'message': 'Nenhum pedido registrado ainda.'})
+            ), 404
+        
+        try:
+            df = pd.read_csv(CSV_PATH, encoding="latin-1")
+            if df.empty:
+                return make_response(
+                    jsonify({'message': 'Nenhum pedido registrado ainda.'})
+                ), 404
+
+            ranking = (
+                df['restaurante']
+                .value_counts()
+                .head(quantidade)
+                .reset_index()
+                .rename(columns={
+                    'restaurante': 'nome_restaurante',
+                    'count': 'quantidade_pedidos'
+                })
+                .to_dict(orient='records')
+            )
+
+            res = make_response(
+                    jsonify({
+                    'message': 'Ranking de restaurantes gerado com sucesso!',
+                    'data': ranking
+                })
+            )
+
+            return res, 200
+        except Exception as e:
+            return make_response(
+                jsonify({'message': 'Erro ao gerar o relatório.', 'error': str(e)})
+            ), 500
+    

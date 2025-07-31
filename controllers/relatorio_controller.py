01eaa9e5cc5ff12a5e42c4d2d2750cfd5bb55c8f
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-07-31 16:07:59 -0300

    feat: implementa endpoint GET /top-restaurantes para obter os ranking dos restaurantes com mais pedidos

--- controllers/relatorio_controller.py
+++ controllers/relatorio_controller.py
@@ -1,3 +1,11 @@
-from flask import request, Blueprint
+from flask import Blueprint, request
+from services.relatorio_service import RelatorioService
 
 relatorio_bp = Blueprint("ralatorio", __name__)
+service = RelatorioService()
+
+@relatorio_bp.get('/relatorio/top-restaurantes')
+def get_top_restaurantes():
+    quantidade = request.args.get('quantidade', default=10, type=int)
+    res = service.gerar_relatorio_top_restaurante(quantidade)
+    return res
